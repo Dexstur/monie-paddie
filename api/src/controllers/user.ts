190273c566abe39d06145c5cf75be014640b332a
Desmondflexy <ikilodesmond@gmail.com>
2023-11-02 21:47:31 +0100

    update

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -1,16 +1,13 @@
 import User from '../models/user';
 import { Request, Response } from 'express';
 import { generateToken } from "../utils/utils";
-import dev from '../utils/logs';
 
 
 
 export async function login(req: Request, res: Response) {
   try {
     if (req.url.startsWith("/google/redirect?code=")) {
-      // res.send('you reached the callback URI'); return;
       // login with google
-      dev.log(req.user)
       const token = generateToken(req.user, res);
       const clientUrl = process.env.NODE_ENV === "development"? process.env.CLIENT_URL_DEV : process.env.CLIENT_URL;
       return res.redirect(`${clientUrl}/sso?token=${token}`);
