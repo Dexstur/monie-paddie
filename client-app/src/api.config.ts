d092d79aeacde069b1515312b538a879bbc8f7be
Dexstur <chuks89@live.com>
2023-11-17 11:22:40 +0100

    live api added

--- client-app/src/api.config.ts
+++ client-app/src/api.config.ts
@@ -4,7 +4,7 @@ const { VITE_APP_NODE_ENV } = import.meta.env;
 const baseURL =
   VITE_APP_NODE_ENV === "development"
     ? "http://localhost:5500"
-    : "https://tobedecided.onrender.com/";
+    : "https://moniepaddie-api.onrender.com";
 const Api = axios.create({
   baseURL,
   withCredentials: true,
